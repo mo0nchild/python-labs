a79547c8a57d4b1ab8aecb251f5e5707f7cb01ca
mo0nchild <happytown77831@gmail.com>
2023-12-19 23:01:29 +0300

    end

--- control-work/logic.py
+++ control-work/logic.py
@@ -19,10 +19,21 @@ class RecipeModel(object):
     def display_info(self):
         return f'{self.__name} {self.__category} {self.__description}'
 
-recipes = [
-    RecipeModel("Мисо_суп", "Первое_блюдо", "Вам_понравится"),
-    RecipeModel("Стейк", "Десерт", "Чистый_протеин"),
-    RecipeModel("Кофе", "Напиток", "Поможет_не_спать")
-]
+
+recipe1 = RecipeModel("Кофе", "Напиток", "Поможет_не_спать")
+recipe1.ingredients.append("Молоко")
+recipe1.ingredients.append("Кофе")
+recipe1.ingredients.append("Чашка")
+
+recipe2 = RecipeModel("Мисо_суп", "Первое_блюдо", "Вам_понравится")
+recipe2.ingredients.append("Вода")
+recipe2.ingredients.append("Сыр")
+recipe2.ingredients.append("Чашка")
+
+recipe3 = RecipeModel("Стейк", "Десерт", "Чистый_протеин")
+recipe3.ingredients.append("Мясо")
+recipe3.ingredients.append("Специи")
+
+recipes = [recipe1, recipe2, recipe3]
 categories = ["Первые блюда", "Вторый блюда", "Десерты", "Выпечка", "Напитки"]
 
--- control-work/main.py
+++ control-work/main.py
@@ -1,6 +1,7 @@
 import tkinter as Tk
 from tkinter import ttk
 import tkinter.font as font
+from tkinter import messagebox
 import logic
 
 class CookingRecipeApp(object):
@@ -13,7 +14,7 @@ class CookingRecipeApp(object):
         self.root.resizable(False, False)
 
         columns = 'Название', 'Категория', 'Описание'
-        self.tree = ttk.Treeview(root, columns=columns, show='headings')
+        self.tree = ttk.Treeview(root, columns=columns, show='headings', selectmode="browse")
         self.tree.heading('Название', text='Название')
         self.tree.heading('Категория', text='Категория')
         self.tree.heading('Описание', text='Описание')
@@ -21,6 +22,8 @@ class CookingRecipeApp(object):
         self.tree.column('Название', width=100, anchor=Tk.W)
         self.tree.column('Категория', width=100, anchor=Tk.W)
         self.tree.column('Описание', width=100, anchor=Tk.W)
+        self.tree.bind("<<TreeviewSelect>>", self.on_treeview_select)
+        self.selectedIndex = None
 
         self.tree.pack(side=Tk.LEFT, fill=Tk.BOTH)
         for item in logic.recipes:
@@ -36,12 +39,22 @@ class CookingRecipeApp(object):
         label1 = Tk.Label(entryFrame, text="Название рецепта:")
         label1.pack()
         self.nameEntry = Tk.Entry(entryFrame, font=('sans-serif 12'))
+        self.nameEntry.bind("<FocusIn>", lambda args: self.on_entry_click(self.nameEntry,
+                                                                                 "Укажите название"))
+        self.nameEntry.bind("<FocusOut>", lambda args: self.on_focus_out(self.nameEntry,
+                                                                                "Укажите название"))
         self.nameEntry.pack()
 
         label2 = Tk.Label(entryFrame, text="Описание:")
         label2.pack(pady=(10, 0))
         self.descriptionEntry = Tk.Entry(entryFrame, font=('sans-serif 12'))
+        self.descriptionEntry.bind("<FocusIn>", lambda args: self.on_entry_click(self.descriptionEntry,
+                                                                    "Укажите описание"))
+        self.descriptionEntry.bind("<FocusOut>", lambda args: self.on_focus_out(self.descriptionEntry,
+                                                                    "Укажите описание"))
         self.descriptionEntry.pack()
+        self.on_focus_out(self.nameEntry, "Укажите название")
+        self.on_focus_out(self.descriptionEntry, "Укажите описание")
 
         label = ttk.Label(entryFrame, text="Категория:")
         label.pack(pady=(10, 0))
@@ -53,25 +66,94 @@ class CookingRecipeApp(object):
         # добавляем фреймы в качестве вкладок
         notebook.add(entryFrame, text="Конфигурация")
 
-        ingredientsFrame = Tk.Frame(root)
+        ingredientsFrame = Tk.Frame(notebook)
         ingredientsFrame.pack(fill=Tk.BOTH, expand=True)
+
+        self.ingredients_listbox = Tk.Listbox(ingredientsFrame, selectmode=Tk.SINGLE, font=("sans-serif", 10))
+        self.ingredients_listbox.pack(pady=10)
+
+        # Создаем поля для ввода названия и количества ингредиента
+        ttk.Label(ingredientsFrame, text="Название ингредента:").pack(pady=(10, 0))
+        self.name_entry = Tk.Entry(ingredientsFrame, width=20, font=("sans-serif", 12))
+        self.name_entry.pack(pady=5)
+
+        # Создаем кнопки для добавления и удаления ингредиентов
+        self.add_button = Tk.Button(ingredientsFrame, text="Добавить", command=self.add_ingredient)
+        self.add_button.pack(side=Tk.LEFT, padx=5)
+        self.remove_button = Tk.Button(ingredientsFrame, text="Убрать", command=self.remove_ingredient)
+        self.remove_button.pack(side=Tk.LEFT, padx=5)
 
         notebook.add(ingredientsFrame, text="Ингредиенты")
 
         buttonFrame = Tk.Frame(root)
         buttonFrame.pack(side=Tk.BOTTOM, pady=20)
 
-        self.infoButton = ttk.Button(buttonFrame, text="Посмотреть рецепт",
-                                     command=self.addRecipeHandler)
-        self.infoButton.pack(side=Tk.TOP, pady=5, fill=Tk.BOTH)
-
-        self.addButton = ttk.Button(buttonFrame, text="Добавить рецепт",
-                                    command=self.addRecipeHandler)
+        self.addButton = Tk.Button(buttonFrame, text="Добавить рецепт", font=("sans-serif", 10),
+                                    bg="white", command=self.addRecipeHandler)
         self.addButton.pack(side=Tk.RIGHT, padx=5)
 
-        self.deleteButton = ttk.Button(buttonFrame, text="Удалить запись",
-                                       command=self.deleteRecipeHandler)
+        self.deleteButton = Tk.Button(buttonFrame, text="Удалить запись", font=("sans-serif", 10),
+                                       bg="white", command=self.deleteRecipeHandler)
         self.deleteButton.pack(side=Tk.RIGHT, padx=5)
+
+    def add_ingredient(self):
+        if self.selectedIndex is None:
+            messagebox.showerror("Ошибка", "Рецепт не выбран")
+            return
+        # Получаем значения из полей ввода
+        name = self.name_entry.get()
+
+        # Проверяем, что оба поля заполнены
+        if not name:
+            messagebox.showwarning("Ошибка", "Введите название ингредиента")
+            return
+
+        # Добавляем ингредиент в список и отображаем в Listbox
+        self.ingredients_listbox.insert(Tk.END, name)
+        logic.recipes[self.selectedIndex].ingredients.append(name)
+        # Очищаем поля ввода
+        self.name_entry.delete(0, Tk.END)
+
+    def remove_ingredient(self):
+        # Получаем индекс выбранного элемента
+        selected_index = self.ingredients_listbox.curselection()
+
+        # Проверяем, что элемент выбран
+        if not selected_index:
+            return
+
+        # Удаляем ингредиент из списка и Listbox
+        self.ingredients_listbox.delete(selected_index)
+        print(selected_index)
+        logic.recipes[self.selectedIndex].ingredients.pop(selected_index[0])
+
+    def on_entry_click(self, entry, placeholder):
+        if entry.get() == placeholder:
+            entry.delete(0, Tk.END)
+            entry.config(fg='black')  # Change text color to black
+
+    def on_focus_out(self, entry, placeholder):
+        if not entry.get():
+            entry.insert(0, placeholder)
+            entry.config(fg='grey')  # Change text color to grey
+
+    def on_treeview_select(self, event):
+        selected_items = self.tree.selection()
+        selected_indices = []
+
+        for item in selected_items:
+            selected_indices.append(self.tree.index(item))
+
+        if len(selected_indices) > 0:
+            self.selectedIndex = selected_indices[0]
+            self.ingredients_listbox.delete(0, Tk.END)
+            index = 0
+            for ingredient in logic.recipes[self.selectedIndex].ingredients:
+                self.ingredients_listbox.insert(index, ingredient)
+                index+=1
+        else: self.selectedIndex = None
+
+        print(f"Selected Indices: {selected_indices}")
 
     def update_view(self):
         for item in self.tree.get_children():
@@ -81,14 +163,31 @@ class CookingRecipeApp(object):
             self.tree.insert('', Tk.END, values=item.display_info())
 
     def addRecipeHandler(self):
-        newRecipe = logic.RecipeModel(self.nameEntry.get().replace(" ", "_"),
-                                      self.categoryCombobox.get().replace(" ", "_"),
-                                      self.descriptionEntry.get().replace(" ", "_"))
+        recipeName = self.nameEntry.get().replace(" ", "_")
+        recipeDescription = self.descriptionEntry.get().replace(" ", "_")
+
+        if(len(recipeName) <= 0 or len(recipeDescription) <= 0):
+            messagebox.showerror("Ошибка", "Текстовое поле не заполнено")
+            return
+
+        for recipe in logic.recipes:
+            if recipe.name == recipeName:
+                messagebox.showerror("Ошибка", "Рецепт с таким название существует")
+                return
+
+        newRecipe = logic.RecipeModel(name=recipeName, description=recipeDescription,
+                                      category=self.categoryCombobox.get().replace(" ", "_"), )
         logic.recipes.append(newRecipe)
         self.update_view()
+        self.nameEntry.delete(0, Tk.END)
+        self.descriptionEntry.delete(0, Tk.END)
+        # self.on_focus_out(self.nameEntry, "Укажите название")
+        # self.on_focus_out(self.descriptionEntry, "Укажите описание")
 
     def deleteRecipeHandler(self):
-        logic.recipes.pop()
+        if self.selectedIndex is None:
+            return
+        logic.recipes.pop(self.selectedIndex)
         self.update_view()
 
 
